e1aa848720d512e5559ee0e4316b9415a29e0f68
adii1203 <aditya32ft@gmail.com>
2024-11-20 14:02:12 +0530

    pointers and errors

--- pointers_errors/main.go
+++ pointers_errors/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -24,10 +27,41 @@ func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
 	w.balance -= amount
+	return nil
 }
 
 func main() {
 	fmt.Println("Hello, playground")
 }
+
+// Pointers
+//Go copies values when you pass them to functions/methods,
+//so if you're writing a function that needs to mutate state you'll need it to take a pointer to the thing you want to change.
+//
+//The fact that Go takes a copy of values is useful a lot of the time,
+//but sometimes you won't want your system to make a copy of something, in which case you need to pass a reference.
+//Examples include referencing very large data structures or things where only one instance is necessary (like database connection pools).
+
+// nil
+// Pointers can be nil
+//
+// When a function returns a pointer to something, you need to make sure you check if it's nil, or you might raise a runtime exception - the compiler won't help you here.
+//
+// Useful for when you want to describe a value that could be missing
+
+//Errors
+//Errors are the way to signify failure when calling a function/method.
+//
+//By listening to our tests we concluded that checking for a string in an error would result in a flaky test. So we refactored our implementation to use a meaningful value instead and this resulted in easier to test code and concluded this would be easier for users of our API too.
+//
+//This is not the end of the story with error handling, you can do more sophisticated things but this is just an intro. Later sections will cover more strategies.
+//
+//Don’t just check errors, handle them gracefully
--- pointers_errors/main_test.go
+++ pointers_errors/main_test.go
@@ -5,14 +5,8 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-	t.Run("Deposite", func(t *testing.T) {
+
+	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
@@ -20,7 +14,43 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, Bitcoin(20))
+	})
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got == nil {
+		t.Fatal("Wanted an error but didn't get one")
+	}
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
 }
